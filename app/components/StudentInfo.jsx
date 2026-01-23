3fdeb30c35f4c47161b677b229982615c8cd4dd3
Louis Li <lilu@asv.ai>
2026-01-22 18:51:22 -0700

    add Week 2 Page + StudentInfo Component and Main Page Setup

---
+++ app/components/StudentInfo.jsx
@@ -0,0 +1,14 @@
+
+export default function StudentInfo() {
+  return (
+    <section>
+      <h2>Student Info</h2>
+      <ul>
+        <li>Name: Lu Li</li>
+        <li>Student ID: 000947735</li>
+        <li>Course: CPRG 306-D</li>
+        <li>Instructor: Ashlyn Knox</li>
+      </ul>
+    </section>
+  );
+}
---
+++ app/week-2/page.jsx
@@ -0,0 +1,12 @@
+import StudentInfo from "../components/StudentInfo";
+
+export default function Week2Page() {
+  return (
+    <main>
+      <h1>Week 2 Assignment</h1>
+      <p>This page displays the StudentInfo component.</p>
+
+      <StudentInfo />
+    </main>
+  );
+}
