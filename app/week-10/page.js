54eea85f86b8591a078be9feb0c8d94e49276de3
Louis Li <lilu@asv.ai>
2026-05-05 19:46:02 -0600

    test2

--- app/week-10/page.js
+++ app/week-10/page.js
@@ -8,7 +8,7 @@ export default function Page() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-6 p-6">
-      <h1 className="text-3xl font-bold">Week 10 — Cloud Firestore</h1>
+      <h1 className="text-3xl font-bold">Week 11 — Cloud 1 Firestore</h1>
 
       {user ? (
         <div className="flex flex-col items-center gap-4">
